5f5c0554b3746e0b0ed6df245a5444c8ced93a8d
wangpbj <wangpbj@cn.ibm.com>
2017-08-23 16:16:18 +0800

    add build file

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../all-spark-notebook
+cd all-spark-notebook
 
 #wget https://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
 cp /opt/dependencies/spark-2.0.2-bin-hadoop2.7.tgz .
@@ -9,6 +9,6 @@ tar -zxvf spark-2.0.2-bin-hadoop2.7.tgz
 
 mkdir spark && mv spark-2.0.2-bin-hadoop2.7/* spark/
 
-docker build -t birdstar/all-spark-notebook-kg:v2.0 -f all-spark-notebook/Dockerfile.kernelgateway.root.dist .
+docker build -t birdstar/all-spark-notebook-kg:v2.0 -f Dockerfile.kernelgateway.root.dist .
 
 
