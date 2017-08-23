8e4f6cbbe24e704759d518f1888528e71e3d6f1f
wangpbj <wangpbj@cn.ibm.com>
2017-08-23 16:00:37 +0800

    add build file

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,8 @@
 
 cd ../all-spark-notebook
 
-wget https://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
+#wget https://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
+cp /opt/dependencies/spark-2.0.2-bin-hadoop2.7.tgz .
 
 tar -zxvf spark-2.0.2-bin-hadoop2.7.tgz
 
