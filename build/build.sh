bea3f8f8681a855bebbe245c241743497c2256d4
wangpbj <wangpbj@cn.ibm.com>
2017-08-29 17:38:14 +0800

    fix build

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+timestamp=$(date +%Y%m%d.%H%M%S)
+
+mkdir "${ARTIFACT_DIR}"
+
 cd all-spark-notebook
 
 #wget https://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
@@ -13,6 +17,9 @@ docker build -t birdstar/all-spark-notebook-kg:v2.0 -f Dockerfile.kernelgateway.
 
 mkdir -p /tmp/docker-stacks/
 
-docker save birdstar/all-spark-notebook-kg:v2.0 | gzip -c > /tmp/docker-stacks/kg.tar.gz
+docker save birdstar/all-spark-notebook-kg:v2.0 | gzip -c > docker-stacks/kg.tar.gz
+
+tar -cvzf ${WORKSPACE}/${REPO}.${MY_VER}-${timestamp}.tgz -o "${REPO}-artifact"
+
 
 
