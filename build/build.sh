9dbf37a31b12e0278d63763d58385ad59ef9d0d8
wangpbj <wangpbj@cn.ibm.com>
2017-08-30 09:40:28 +0800

    fix build

--- build/build.sh
+++ build/build.sh
@@ -9,11 +9,11 @@ mkdir -p /tmp/docker-stacks
 cd all-spark-notebook
 
 #wget https://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
-cp /opt/dependencies/spark-2.0.2-bin-hadoop2.7.tgz .
+#cp /opt/dependencies/spark-2.0.2-bin-hadoop2.7.tgz .
 
-tar -zxvf spark-2.0.2-bin-hadoop2.7.tgz
+#tar -zxvf spark-2.0.2-bin-hadoop2.7.tgz
 
-mkdir spark && mv spark-2.0.2-bin-hadoop2.7/* spark/
+#mkdir spark && mv spark-2.0.2-bin-hadoop2.7/* spark/
 
 docker build -t birdstar/all-spark-notebook-kg:v2.0 -f Dockerfile.kernelgateway.root.dist .
 
