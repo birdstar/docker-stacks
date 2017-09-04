52a4116f6af66ef958452aa6e797705be0eec342
wangpbj <wangpbj@cn.ibm.com>
2017-09-04 10:42:44 +0800

    upgrade to spark2.1.1

--- build/build.sh
+++ build/build.sh
@@ -18,25 +18,15 @@ kg_image_name="${local_docker_server}/all-spark-notebook-kg:${kg_image_tag}"
 cd all-spark-notebook
 
 #wget https://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
-cp /opt/dependencies/spark-2.0.2-bin-hadoop2.7.tgz .
+cp /opt/dependencies/spark-2.1.1-bin-hadoop2.7.tgz .
 
-tar -zxvf spark-2.0.2-bin-hadoop2.7.tgz
+tar -zxvf spark-2.1.1-bin-hadoop2.7.tgz
 
-mkdir spark && mv spark-2.0.2-bin-hadoop2.7/* spark/
+mkdir spark && mv spark-2.1.1-bin-hadoop2.7/* spark/
 
 docker build -t ${kg_image_name} -f Dockerfile.kernelgateway.root.dist .
 
 cd ../
-
-
-echo "ls ${REPO}-artifact"
-ls ${REPO}-artifact
-echo ${REPO}-artifact
-
-echo "ls ${ARTIFACT_DIR}"
-ls ${ARTIFACT_DIR}
-echo ${ARTIFACT_DIR}
-
 
 #docker save birdstar/all-spark-notebook-kg:v2.0 | gzip -c > ${REPO}-artifact/${kg_image_short_name}_${kg_image_tag}.tar.gz
 docker save ${kg_image_name} | gzip -c > /tmp/${PROJECT_NAME}/${kg_image_short_name}_${kg_image_tag}.tar.gz
